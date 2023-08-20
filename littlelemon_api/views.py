6cab3784d20a435261e5731eb4b252acc2f7513d
keyxcode <keyxcode@gmail.com>
2023-08-20 22:05:25 +0700

    Refactor managers fbv

--- littlelemon_api/views.py
+++ littlelemon_api/views.py
@@ -1,29 +1,37 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
+from rest_framework import generics, status
 from django.contrib.auth.models import User, Group
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+
 from .serializers import UserSerializer
 
 
-# Create your views here.
 @api_view(["GET", "POST"])
 @permission_classes([IsAdminUser])
 def managers(request):
     if request.method == "GET":
         users = User.objects.filter(groups__name="Manager")
         serializer = UserSerializer(users, many=True)
+
         return Response(serializer.data)
 
     if request.method == "POST":
-        username = request.data["username"]
+        username = request.POST.get("username", None)
 
         if username:
             user = get_object_or_404(User, username=username)
             managers = Group.objects.get(name="Manager")
             managers.user_set.add(user)
-            return Response({"message": "ok"})
 
-    return Response({"message": "error"})
+            return Response(
+                {"message": f"assigned user {user} to group Manager"},
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(
+            {"message": "missing username"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    return Response({"message": "unknown error"})
